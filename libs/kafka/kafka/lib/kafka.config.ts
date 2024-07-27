f9af58dd8945143aacd438631f3cd90eeda05845
Dima <zimoykin@gmail.com>
2024-07-27 19:24:18 +0200

    config updated

--- libs/kafka/kafka/lib/kafka.config.ts
+++ libs/kafka/kafka/lib/kafka.config.ts
@@ -1,9 +1,6 @@
 import * as Joi from "joi";
 
-const schema = {
-    PORT: Joi.number().min(3000).max(9000).default(3000),
+export const kafka_schema = {
     KAFKA_CLIENT_ID: Joi.string().default('kafka-cloud-client'),
     KAFKA_BROKERS: Joi.string().default('localhost:29092'),
 };
-export const KafkaCloudSchema = Joi.object(schema);
-export type KafkaCloudConfigVariables = typeof schema;
