881ef5fc595d83646112746b305cbc56ce0f60fa
Dima <zimoykin@gmail.com>
2024-07-27 18:24:25 +0200

    kafka.config added

--- libs/kafka/index.ts
+++ libs/kafka/index.ts
@@ -2,3 +2,4 @@ export * from './kafka/lib/kafka.module';
 export * from './kafka/lib/kafka.consumer';
 export * from './kafka/lib/kafka.producer';
 export * from './kafka/lib/decorators';
+export * from './kafka/lib/kafka.config';
