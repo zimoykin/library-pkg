d44f0b55a48f0552f2aa13b7a4ce5c1010b2f336
Dima <zimoykin@gmail.com>
2024-07-30 17:33:26 +0200

    redis export fix

--- libs/redis/lib/redis.config.ts
+++ libs/redis/lib/redis.config.ts
@@ -1,6 +1,6 @@
 import * as Joi from "joi";
 
-export const kafka_schema = {
+export const redis_schema = {
     REDIS_HOST: Joi.string().default('127.0.0.1'),
     REDIS_PORT: Joi.string().default('6379'),
     REDIS_PASSWORD: Joi.string().default('redis-password'),
