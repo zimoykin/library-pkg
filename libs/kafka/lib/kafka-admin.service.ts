9b3520c307df700610e5a1b4a7a8efa07f625f7e
Dima <zimoykin@gmail.com>
2024-08-02 15:25:39 +0200

    kafka: extend admin kafka service

--- libs/kafka/lib/kafka-admin.service.ts
+++ libs/kafka/lib/kafka-admin.service.ts
@@ -1,22 +1,75 @@
 import { Inject, Injectable, Logger } from "@nestjs/common";
-import { Kafka } from "kafkajs";
+import { Admin, Kafka } from "kafkajs";
 
 @Injectable()
 export class KafkaAdminService {
     private readonly logger = new Logger(KafkaAdminService.name);
+    private admin?: Admin;
     constructor(
         @Inject('KAFKA_CONNECTION') private readonly kafka: Kafka,
     ) { }
 
+    onModuleInit() {
+        this.admin = this.kafka.admin();
+    }
+
     async getTopics() {
-        const topics = await this.kafka.admin().listTopics();
+        if (!this.admin) {
+            throw new Error('KAFKA: Admin not initialized');
+        }
+        const topics = await this.admin.listTopics();
         return topics;
     }
 
-    async getTopicstopicsMetadata(topicName: string) {
-        const topicsMetadata = await this.kafka.admin().fetchTopicMetadata({
+    async getGroups() {
+        if (!this.admin) {
+            throw new Error('KAFKA: Admin not initialized');
+        }
+        const groups = await this.admin.listGroups();
+        return groups;
+    }
+
+    async getTopicMetadata(topicName: string) {
+        if (!this.admin) {
+            throw new Error('KAFKA: Admin not initialized');
+        }
+        const topicMetadata = await this.admin.fetchTopicMetadata({
             topics: [topicName]
         });
-        return topicsMetadata;
+        return topicMetadata;
+    }
+
+    async deleteTopic(topicName: string) {
+        if (!this.admin) {
+            throw new Error('KAFKA: Admin not initialized');
+        }
+        await this.admin.deleteTopics({
+            topics: [topicName]
+        });
+    }
+
+    async createTopic(topicName: string) {
+        if (!this.admin) {
+            throw new Error('KAFKA: Admin not initialized');
+        }
+        await this.admin.createTopics({
+            topics: [{
+                topic: topicName
+            }]
+        });
+    }
+
+    async deleteGroup(groupId: string) {
+        if (!this.admin) {
+            throw new Error('KAFKA: Admin not initialized');
+        }
+        await this.admin.deleteGroups([groupId]);
+    }
+
+    async fetchTopicOffsets(topic: string) {
+        if (!this.admin) {
+            throw new Error('KAFKA: Admin not initialized');
+        }
+        return this.admin.fetchTopicOffsets(topic);
     }
 }
