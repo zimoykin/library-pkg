a067603dd463cf07346ba25738acc484e65dab34
Dima <zimoykin@gmail.com>
2024-08-05 16:32:47 +0200

    added user model and repository

--- libs/models/index.ts
+++ libs/models/index.ts
@@ -3,3 +3,4 @@ export * from './lib/bar/bar.model';
 export * from './lib/foo/foo.model';
 export * from './lib/quizz/quizz.model';
 export * from './lib/topics.enum';
+export * from './lib/user';
---
+++ libs/models/lib/user/user.schema.ts
@@ -0,0 +1,49 @@
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+import { BaseModel } from "../mongo-base-model";
+import { USER_ROLE } from "./enums/user-role.enum";
+
+@Schema({
+    timestamps: true,
+    collection: 'users'
+})
+class User extends BaseModel {
+    @Prop({ unique: true })
+    email: string;
+
+    @Prop()
+    name: string;
+
+    @Prop()
+    url: string;
+
+    @Prop({ enum: USER_ROLE, required: true, default: USER_ROLE.USER })
+    role: USER_ROLE;
+
+    @Prop({ required: true, default: false })
+    confirmed: boolean;
+
+    @Prop({ default: null, type: Date, required: false })
+    blocked?: Date;
+
+    constructor(data: any) {
+        super();
+        this.email = data?.email;
+        this.name = data?.name;
+        this.url = data?.url;
+        this.role = data?.role;
+        this.confirmed = data?.confirmed;
+    }
+}
+
+const UserSchema = SchemaFactory.createForClass(User);
+UserSchema
+    .index({ email: 1 })
+    .index({ createdAt: 1 }, {
+        partialFilterExpression: { confirmed: false },
+        expireAfterSeconds: 60 * 5,
+        name: 'deleting non confirmed users'
+    });
+
+
+
+export { User, UserSchema };
