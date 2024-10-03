560eb8cc45fe40fedc9340933ddfc80d6b750d39
Dima <zimoykin@gmail.com>
2024-10-03 22:54:47 +0200

    added docs

--- libs/amqp/lib/amqp.sender.ts
+++ libs/amqp/lib/amqp.sender.ts
@@ -2,7 +2,7 @@ import { Inject, Injectable, Logger, OnModuleDestroy, OnModuleInit } from '@nest
 import * as amqplib from 'amqplib';
 
 @Injectable()
-export class AmqpSender implements OnModuleInit, OnModuleDestroy {
+export class AmqpSender implements OnModuleDestroy {
     private readonly logger = new Logger(AmqpSender.name);
     private channel: amqplib.Channel;
 
@@ -13,8 +13,20 @@ export class AmqpSender implements OnModuleInit, OnModuleDestroy {
         @Inject('AMQP_PATTERN') private readonly pattern: string
     ) { }
 
-    async onModuleInit() {
-        this.logger.debug(`Init Sender to ${this.pattern}`);
+    /**
+     * Initialize the AMQP channel for this sender.
+     * 
+     * This method ensures that the channel is initialized and the queue is declared.
+     * If the channel is already initialized, this method does nothing.
+     * 
+     * @returns A promise that resolves when the channel is initialized.
+     */
+    
+    async initChannel() {
+        if (this.channel) {
+            return;
+        }
+        this.logger.debug(`Initialize AMQP-Sender to ${this.pattern}`);
         const channel = await this.connection.createConfirmChannel();
         channel.prefetch(1);
         channel.assertQueue(this.pattern, {
@@ -24,12 +36,21 @@ export class AmqpSender implements OnModuleInit, OnModuleDestroy {
     }
 
     async onModuleDestroy() {
-        this.logger.debug(`Destroy Sender to ${this.pattern}`);
+        this.logger.debug(`Destroy AMQP-Sender to ${this.pattern}`);
         this.channel?.close();
     }
 
 
+    /**
+     * Sends a message to the specified queue.
+     * 
+     * This method will first initialize the channel if it is not already initialized.
+     * Then it will send the message to the queue and log any errors.
+     * 
+     * @param message - The message to send.
+     */
     async sendMessage(message: Record<string, any>) {
+        this.initChannel();
         try {
             this.channel?.sendToQueue(this.pattern, Buffer.from(JSON.stringify(message)), {
                 persistent: true
