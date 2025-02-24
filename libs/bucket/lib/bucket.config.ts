1965390111038b1625dda94ad8fe4d2bdddd6834
Dima <zimoykin@gmail.com>
2025-02-24 15:19:55 +0100

    feat(bucket): add CL_ACCOUNT_ID to bucket schema validation

--- libs/bucket/lib/bucket.config.ts
+++ libs/bucket/lib/bucket.config.ts
@@ -1,8 +1,8 @@
 import * as Joi from "joi";
 
 export const bucket_schema = {
-    CL_TOKEN: Joi.string().required(),
     CL_ACCESS_ID: Joi.string().required(),
     CL_SECRET_KEY: Joi.string().required(),
     CL_ENDPOINT: Joi.string().required(),
+    CL_ACCOUNT_ID: Joi.string().required(),
 };
